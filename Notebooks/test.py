837e02d1f9cac7e455bd04f3ae037ec1e015ed5a
Zhaoyu Huai <zhaoyuhuai@Zhaoyus-MacBook-Air.local>
2023-05-10 17:38:02 -0400

    python notebooks and data from the local machine

---
+++ Notebooks/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+import sys
+
+name = sys.argv[1]
+print('/datax/scratch/{}'.format(name))
---
+++ Notebooks/test_gpu.py
@@ -0,0 +1,3 @@
+import numpy as np
+from numba import jit, cuda
+from timeit import default_timer as timer 
