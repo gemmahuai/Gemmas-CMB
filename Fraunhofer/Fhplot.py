c57879ac4ff56bae2e60f15724d1dfbd95007c52
Zhaoyu Huai <zhaoyuhuai@Zhaoyus-MacBook-Air.local>
2023-05-11 10:01:23 -0400

    a

--- Fraunhofer/Fhplot.py
+++ Fraunhofer/Fhplot.py
@@ -1,3 +1,7 @@
+### uses Fhcalc.py 
+### generates a perfectly Gaussian illuminated aperture, truncated by a circle, place among a square screen.
+###
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -13,35 +17,41 @@ import Fhcalc
 
 if (len(sys.argv)!=4):
     print('Wrong inputs!')
-    print('Usage: python FRBplot.py output_filename N_screen N_theta_interp')
+    print('Usage: python Fhplot.py output_filename N_screen N_theta_interp')
     sys.exit()
 
-name = sys.argv[1] #file name
+# name of the output file
+name = sys.argv[1]
+# number of pixels in the E-field screen - N_screen by N_screen
 N_screen = np.int(sys.argv[2])
+# number of points to interpolate - N_theta by N_theta
 N_theta = np.int(sys.argv[3])
 
-starttime = dt.datetime.now()
+# starttime = dt.datetime.now() # timeit
+# initialize a screen
 screen = {}
 screen['N'] = N_screen
-screen['D'] = 10
+screen['D'] = 10 # screen diameter 10m
 Fhcalc.Initialize(screen)
 center = (screen['D']/2, screen['D']/2)
 Fhcalc.MultByGaussian(screen, center, 1.0)
-Fhcalc.InCircle(screen, center, 2.0)
+Fhcalc.InCircle(screen, center, 2.0) 
 Fhcalc.ScreenFFT(screen)
 
+# interpolation in sky coordinates
 lam = 0.002 #mm wavelength
 kphot = 2*np.pi/lam
 thetamaxdeg = 2.0
 thetamax = 2*np.pi/180. # 2 degrees in radians
 theta_vec = np.linspace(-thetamax,thetamax,N_theta) 
-# II0 = Fhcalc.Project_I_on_thetagrid(theta_vec,screen,lam) 
+II0 = Fhcalc.Project_I_on_thetagrid(theta_vec,screen,lam) 
+theta_vec = np.linspace(0, 2*thetamax, N_theta)
 # fig = plt.figure(figsize=(8,6))
 # plt.imshow(screen['E'],extent=(0,screen['D'],screen['D'],0))
 # plt.colorbar()
-endtime = dt.datetime.now()
-t = (endtime-starttime).seconds
-print('Time = {:.2f}s'.format(t))
+# endtime = dt.datetime.now()
+# t = (endtime-starttime).seconds
+# print('Time = {:.2f}s'.format(t))
 #plt.savefig('/home/gemma/{}_E0.png'.format(name))
 
 
@@ -56,12 +66,6 @@ Fhcalc.InCircle(screen, center, 2.0) #radius 2.0
 c2 = (center[0]-0.2, center[1]-0.1)
 #c2 = (center[0], center[1])
 Fhcalc.CircleAtten(screen,c2,0.1,1.0*np.exp(np.pi * 1j))
-#c2 = (center[0]+0.1, center[1]+0.3)
-#FRBcalc.CircleAtten(screen,c2,0.1,1/1.3)
-#c2 = (center[0]+0.3, center[1]-0.1)
-#FRBcalc.CircleAtten(screen,c2,0.1,1.3)
-#c2 = (center[0]-0.3, center[1]+0.2)
-#FRBcalc.CircleAtten(screen,c2,0.1,1/1.3)
 Fhcalc.ScreenFFT(screen)
 
 lam = 0.002 #mm wavelength
